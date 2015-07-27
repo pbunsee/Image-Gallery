f29617c2ab412dd6d9ed1db840554ba198f0beb1
Pranesha Bunsee <pbunsee@gmail.com>
2015-07-27 01:15:28 -0400

    Added binding on target #wrapper2 so that user can drag images back to the div from which it was originally sourced
    Streamlined code.

---
+++ app/assets/javascripts/image2_g.js
@@ -0,0 +1,155 @@
+function Gallery(someImageList) {
+  console.log("someImageList is: " + someImageList);
+  this.destinations = _.map(someImageList, function(d){ console.log("aha"); 
+                                                        return new Destination(d); 
+                                                      });
+  this.chirrups = _.map(someImageList, function(d){ console.log("aha"); 
+                                                        return new Chirrup(d); 
+                                                      });
+
+  console.log("this is " + this);
+  console.log("this.destinations is " + this.destinations);
+  this.render = function(){
+    _.each(this.destinations, function(o){
+      o.render();
+    });
+  }
+
+  this.chirpy = function(){
+    _.each(this.chirrups, function(o){
+      o.birdy();
+    });
+  }
+
+}
+
+function Destination(locationShot) {
+  this.source_url = locationShot.source_link;
+  this.source_id = locationShot.id;
+
+  this.render = function(){
+   console.log("pic!");
+   var divID = 'pic-' + this.source_id;
+   console.log(divID);
+   $hotspot = '<div id="' + divID + '" class="pic" draggable="true"></div>';
+   selector = "#"+divID;
+   console.log(selector);
+   console.log($(selector));
+   $('#wrapper').append($hotspot);
+   $(selector).css('background-image', 'url(' + this.source_url + ')');
+   //$(selector).css('draggable', 'true');
+   console.log("hotspot is : " +$hotspot);
+   console.log("is hotspot draggable : " +$(selector).css('draggable'));
+   console.log("hotspot background-image is : " +$(selector).css('background-image'));
+  }
+}
+
+function Chirrup(locationShot) {
+  this.source_url = locationShot.source_link;
+  this.source_id = locationShot.id;
+  $outerChirp = $('<div id="wrapper2"></div>');
+  $('#wrapper2').prepend('<h2>All the birdies in the world unite!</h2>'); 
+  this.birdy = function(){
+    $chirp = $('<div id="chirp-' + this.source_id + '" class="chirp" draggable="true"></div>');
+    $chirp.css('background-image', 'url(' + this.source_url + ')');
+    //$chirp.css('draggable', 'true');
+    $('body').append($outerChirp);
+    //$('.chirp').draggable();
+    //console.log("is chirp draggable: " + $('.chirp').css('draggable'));
+    //$outerChirp.append($chirp);
+    $chirp.appendTo($outerChirp);
+  }
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function drop(e){
+       e.preventDefault();
+       var data = e.dataTransfer.getData("text");
+       e.target.appendChild(document.getElementById(data));
+}
+
+$(document).ready(function() {
+  //console.log( "ready!" );
+  //gallery = new Gallery(source_images);
+  //gallery.render();
+  //gallery.chirpy();
+
+  $('.chirp') 
+    .bind('dragstart', function(evt) { 
+	console.log('in dragstart ' + evt.target.id); 
+        //thingy = evt.target.id;
+        //console.log("thingy: " + thingy); 
+        //thingy2 = $('#' + thingy); 
+        //console.log("thingy2: " + thingy2); 
+        //console.dir(thingy2); 
+        //itembg1 = $(thingy2).css('background-image');
+        //console.log("itembg1: " + itembg1); 
+        evt.originalEvent.dataTransfer.setData('text', evt.target.id); 
+        //evt.dataTransfer.setData('text', evt.target.id); 
+        $('h2').fadeIn('fast'); 
+    }) 
+    .hover( 
+        function () { $('div', this).fadeIn(); },  
+        function () { $('div', this).fadeOut(); } 
+    );
+
+  $('#sandbox') 
+    .bind('dragover', function(evt) { 
+	                //console.log('in dragover'); 
+                        evt.preventDefault(); 
+                      }) 
+    .bind('dragenter', function(evt) { 
+	                 //console.log('in dragenter'); 
+                         evt.preventDefault(); 
+                       }) 
+    .bind('drop', function(evt) { 
+                    $("h2").fadeOut('fast'); 
+                    //var id = evt.dataTransfer.getData('text'), 
+                    var id = evt.originalEvent.dataTransfer.getData('text'); 
+                    item = $('#' + id); 
+                    item.appendTo($('#sandbox'));
+                  })
+
+  $('#sandbox').bind('dragleave', function (evt) { 
+	                            console.log('dragleave'); 
+                                  }); 
+ 
+  $('.chirp') 
+	.bind('dragend', function (evt) { 
+		           console.log('dragend'); 
+	                 }) 
+	.bind('dragstart', function (evt) { 
+		             //console.log('dragstart'); 
+	                   }) 
+	.bind('drag', function (evt) { 
+		        console.log('drag'); 
+	              });
+
+  $('#other-sandbox') 
+    .bind('dragover', function(evt) { 
+                        evt.preventDefault(); 
+                      }) 
+    .bind('dragenter', function(evt) { 
+                         evt.preventDefault(); 
+                       }) 
+    .bind('drop', function(evt) { 
+                    $("h2").fadeOut('fast'); 
+                    var id = evt.originalEvent.dataTransfer.getData('text'); 
+                    item = $('#' + id); 
+                    //item.appendTo($('#other-sandbox'));
+                    getBG = item.css('background-image');
+                    $('#other-sandbox').css('background-image',getBG);
+                  })
+    .bind('dragleave', function (evt) { 
+                         console.log('dragleave'); 
+                       }); 
+
+});
+
--- app/assets/javascripts/image_gallery.js
+++ app/assets/javascripts/image_gallery.js
@@ -1,14 +1,8 @@
 function Gallery(someImageList) {
-  console.log("someImageList is: " + someImageList);
-  this.destinations = _.map(someImageList, function(d){ console.log("aha"); 
-                                                        return new Destination(d); 
-                                                      });
-  this.chirrups = _.map(someImageList, function(d){ console.log("aha"); 
-                                                        return new Chirrup(d); 
-                                                      });
 
-  console.log("this is " + this);
-  console.log("this.destinations is " + this.destinations);
+  this.destinations = _.map(someImageList, function(d){ return new Destination(d); });
+  this.chirrups = _.map(someImageList, function(d){ return new Chirrup(d); });
+
   this.render = function(){
     _.each(this.destinations, function(o){
       o.render();
@@ -24,19 +18,15 @@ function Gallery(someImageList) {
 }
 
 function Destination(locationShot) {
-  this.source_url = locationShot.source_link;
-  this.source_id = locationShot.id;
-
   this.render = function(){
-   console.log("pic!");
-   var divID = 'pic-' + this.source_id;
+   var divID = 'pic-' + locationShot.id;
    console.log(divID);
    $hotspot = '<div id="' + divID + '" class="pic" draggable="true"></div>';
    selector = "#"+divID;
    console.log(selector);
-   console.log($(selector));
+   console.dir($(selector));
    $('#wrapper').append($hotspot);
-   $(selector).css('background-image', 'url(' + this.source_url + ')');
+   $(selector).css('background-image', 'url(' + locationShot.source_link + ')');
    //$(selector).css('draggable', 'true');
    console.log("hotspot is : " +$hotspot);
    console.log("is hotspot draggable : " +$(selector).css('draggable'));
@@ -48,31 +38,55 @@ function Chirrup(locationShot) {
   this.source_url = locationShot.source_link;
   this.source_id = locationShot.id;
   $outerChirp = $('<div id="wrapper2"></div>');
-  $('#wrapper2').prepend('<h2>All the birdies in the world unite!</h2>'); 
   this.birdy = function(){
-    $chirp = $('<div id="chirp-' + this.source_id + '" class="chirp" draggable="true"></div>');
-    $chirp.css('background-image', 'url(' + this.source_url + ')');
-    //$chirp.css('draggable', 'true');
-    $('body').append($outerChirp);
-    //$('.chirp').draggable();
-    //console.log("is chirp draggable: " + $('.chirp').css('draggable'));
-    //$outerChirp.append($chirp);
+    $chirp = $('<div id="chirp-' + locationShot.id + '" class="chirp" draggable="true"></div>');
+    $chirp.css('background-image', 'url(' + locationShot.source_link  + ')');
     $chirp.appendTo($outerChirp);
+    $('body').append($outerChirp);
   }
 }
 
-function allowDrop(e) {
-    e.preventDefault();
+function bindTarget(el) {
+  $(el) 
+    .bind('dragover', function(evt) { 
+                        evt.preventDefault(); 
+                      }) 
+    .bind('dragenter', function(evt) { 
+                         evt.preventDefault(); 
+                       }) 
+    .bind('drop', function(evt) { 
+                    var id = evt.originalEvent.dataTransfer.getData('text'); 
+                    item = $('#' + id); 
+                    console.dir($(el));
+                    if (el == '#other-sandbox') { 
+                      getBG = item.css('background-image');
+                      $(el).css('background-image',getBG);
+                    }
+                    else {
+                      item.appendTo($(el));
+                    }
+                  })
+    .bind('dragleave', function (evt) { 
+                         console.log('dragleave'); 
+                       }); 
 }
 
-function drag(e) {
-    e.dataTransfer.setData("text", e.target.id);
-}
-
-function drop(e){
-       e.preventDefault();
-       var data = e.dataTransfer.getData("text");
-       e.target.appendChild(document.getElementById(data));
+function bindSource(el) {
+  $(el) 
+    .bind('dragstart', function(evt) { 
+	console.log('in dragstart ' + evt.target.id); 
+        evt.originalEvent.dataTransfer.setData('text', evt.target.id); 
+        //evt.dataTransfer.setData('text', evt.target.id); 
+    }) 
+    .bind('dragend', function (evt) { 
+		       console.log('dragend'); 
+	             }) 
+    .bind('dragstart', function (evt) { 
+          	         //console.log('dragstart'); 
+	               }) 
+    .bind('drag', function (evt) { 
+         	    console.log('drag'); 
+	          });
 }
 
 $(document).ready(function() {
@@ -80,76 +94,12 @@ $(document).ready(function() {
   gallery = new Gallery(source_images);
   gallery.render();
   gallery.chirpy();
+  $('#wrapper2').prepend('<p>This is the #wrapper2 div!</p>'); 
+  $('#wrapper2').prepend('<p>These images can be dragged from here!</p>'); 
 
-  $('.chirp') 
-    .bind('dragstart', function(evt) { 
-	console.log('in dragstart ' + evt.target.id); 
-        //thingy = evt.target.id;
-        //console.log("thingy: " + thingy); 
-        //thingy2 = $('#' + thingy); 
-        //console.log("thingy2: " + thingy2); 
-        //console.dir(thingy2); 
-        //itembg1 = $(thingy2).css('background-image');
-        //console.log("itembg1: " + itembg1); 
-        evt.originalEvent.dataTransfer.setData('text', evt.target.id); 
-        //evt.dataTransfer.setData('text', evt.target.id); 
-        $('h2').fadeIn('fast'); 
-    }) 
-    .hover( 
-        function () { $('div', this).fadeIn(); },  
-        function () { $('div', this).fadeOut(); } 
-    );
-
-  $('#sandbox') 
-    .bind('dragover', function(evt) { 
-	                //console.log('in dragover'); 
-                        evt.preventDefault(); 
-                      }) 
-    .bind('dragenter', function(evt) { 
-	                 //console.log('in dragenter'); 
-                         evt.preventDefault(); 
-                       }) 
-    .bind('drop', function(evt) { 
-                    $("h2").fadeOut('fast'); 
-                    //var id = evt.dataTransfer.getData('text'), 
-                    var id = evt.originalEvent.dataTransfer.getData('text'); 
-                    item = $('#' + id); 
-                    item.appendTo($('#sandbox'));
-                  })
-
-  $('#sandbox').bind('dragleave', function (evt) { 
-	                            console.log('dragleave'); 
-                                  }); 
- 
-  $('.chirp') 
-	.bind('dragend', function (evt) { 
-		           console.log('dragend'); 
-	                 }) 
-	.bind('dragstart', function (evt) { 
-		             //console.log('dragstart'); 
-	                   }) 
-	.bind('drag', function (evt) { 
-		        console.log('drag'); 
-	              });
-
-  $('#other-sandbox') 
-    .bind('dragover', function(evt) { 
-                        evt.preventDefault(); 
-                      }) 
-    .bind('dragenter', function(evt) { 
-                         evt.preventDefault(); 
-                       }) 
-    .bind('drop', function(evt) { 
-                    $("h2").fadeOut('fast'); 
-                    var id = evt.originalEvent.dataTransfer.getData('text'); 
-                    item = $('#' + id); 
-                    //item.appendTo($('#other-sandbox'));
-                    getBG = item.css('background-image');
-                    $('#other-sandbox').css('background-image',getBG);
-                  })
-    .bind('dragleave', function (evt) { 
-                         console.log('dragleave'); 
-                       }); 
-
+  bindSource('.chirp');
+  bindTarget('#sandbox');
+  bindTarget('#other-sandbox');
+  bindTarget('#wrapper2');
 });
 
